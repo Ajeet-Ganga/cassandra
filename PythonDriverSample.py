88d8add06f157d1573c59e02c2324072f13b0a49
ajeet <toajeet@yahoo.com>
2014-09-17 22:25:24 -0700

    Update PythonDriverSample.py

--- PythonDriverSample.py
+++ PythonDriverSample.py
@@ -9,16 +9,22 @@ import string
 #  CREATE KEYSPACE keyspace1 WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
 #  CREATE TABLE users (  user_id int PRIMARY KEY,  fname text,  lname text);
 
+# TO Profile run it like
+# python -m profile py.py | grep doit
+
 cluster = Cluster(['10.9.222.224'],protocol_version=1);
 session = cluster.connect();
 session.set_keyspace('keyspace1')
 
-i = 0
-while i < 10:
-        user_id = ''.join([random.choice(string.digits + string.digits) for n in xrange(5)])
-        fname = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
-        lname = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
+def doit():
+        i = 0
+        while i < 10:
+                user_id = ''.join([random.choice(string.digits + string.digits) for n in xrange(5)])
+                fname = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
+                lname = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
+        
+                session.execute("INSERT INTO users (user_id,  fname, lname)  VALUES (%s, %s, %s)",(int(user_id),fname,lname));
+        
+                i = i + 1;
 
-        session.execute("INSERT INTO users (user_id,  fname, lname)  VALUES (%s, %s, %s)",(int(user_id),fname,lname));
-
-        i = i + 1;
+doit();
