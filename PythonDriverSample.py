4046c3e4b68441a2a6711a65ef091a6aec095e39
ajeet <toajeet@yahoo.com>
2014-09-17 22:10:09 -0700

    Create PythonDriverSample.py

---
+++ PythonDriverSample.py
@@ -0,0 +1,24 @@
+from cassandra.cluster import Cluster
+from cassandra.policies import DCAwareRoundRobinPolicy
+import os
+import uuid
+import random
+import string
+
+# First do this once from cqlsh
+#  CREATE KEYSPACE keyspace1 WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+#  CREATE TABLE users (  user_id int PRIMARY KEY,  fname text,  lname text);
+
+cluster = Cluster(['10.9.222.224'],protocol_version=1);
+session = cluster.connect();
+session.set_keyspace('keyspace1')
+
+i = 0
+while i < 10:
+        user_id = ''.join([random.choice(string.digits + string.digits) for n in xrange(5)])
+        fname = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
+        lname = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
+
+        session.execute("INSERT INTO users (user_id,  fname, lname)  VALUES (%s, %s, %s)",(int(user_id),fname,lname));
+
+        i = i + 1;
